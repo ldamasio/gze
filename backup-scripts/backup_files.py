557d28f332e8fca154f768fefa314820a5076157
ldamasio <ldamasio@gmail.com>
2023-11-08 03:58:38 -0300

    Backup

---
+++ backup-scripts/backup_files.py
@@ -0,0 +1,8 @@
+from array_files import files
+
+origin_dir = ''
+
+destination_dir = ''
+
+for i in files:
+    print(i)
---
+++ backup-scripts/count_files.py
@@ -0,0 +1,8 @@
+from array_files import files
+
+print (len(files))
+
+
+
+print (len(files) * 200)
+
